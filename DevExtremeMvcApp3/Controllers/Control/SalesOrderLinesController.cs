fa722f1297b31cb55842722e027908b7d1885a83
admin <admin@DESKTOP-VQQABVQ>
2019-06-04 05:44:39 +0700

    .

--- DevExtremeMvcApp3/Controllers/Control/SalesOrderLinesController.cs
+++ DevExtremeMvcApp3/Controllers/Control/SalesOrderLinesController.cs
@@ -1,8 +1,9 @@
 ﻿using DevExtremeMvcApp3.Models;
 using Newtonsoft.Json;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +25,66 @@ namespace DevExtremeMvcApp3.Controllers.Control
             return PartialView();
 
         }
+        public class C_BaoGiaThongThuong
+        {
+            public string ProductName { get; set; }
+            public string UnitOfMeasureName { get; set; }
+            public double Quantity { get; set; }
+            public double DefaultSellingPrice { get; set; }
+
+        }
+
+        public ActionResult BaoGiaThongThuong(int ID = 0)
+        {
+             string path = System.Web.HttpContext.Current.Server.MapPath("~") +  "BAOGIATHONGTHUONG.xlsx";
+
+             using (System.IO.FileStream stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                using (ExcelPackage excel = new ExcelPackage(stream))
+                {
+                     // Get Sheet Name
+                    var workbook = excel.Workbook;
+                    var ws = workbook.Worksheets.FirstOrDefault();
+                    int recordRow = 12;
+                    using (Models.VTEntities db = new VTEntities())
+                    {
+                        var data = db.Database.SqlQuery<C_BaoGiaThongThuong>(@"
+                                SELECT T2.ProductName,T3.UnitOfMeasureName,T1.Quantity,T2.DefaultSellingPrice 
+                                FROM SalesOrderLine as t1
+                                inner join Product as t2 on t1.ProductId = t2.ProductId
+
+                                left join UnitOfMeasure as t3 on t2.UnitOfMeasureId = t3.UnitOfMeasureId
+
+                                where t1.SalesOrderId = " + ID + "").ToList();
+
+                        foreach (var item in data)
+                        {
+                         ws = CoppyRow(ws, 12, recordRow);
+                            ws.Cells[recordRow, 2].Value = item.ProductName;
+                            ws.Cells[recordRow, 3].Value = item.UnitOfMeasureName;
+                            ws.Cells[recordRow, 4].Value = item.Quantity;
+                            ws.Cells[recordRow, 5].Value = item.DefaultSellingPrice;
+                            ws.InsertRow(recordRow,1);
+
+                            recordRow++;
+                        }
+                        var byteexcel = excel.GetAsByteArray();
+                        System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                        var TENKH = db.Database.SqlQuery<String>("SELECT t2.CustomerName FROM SalesOrder as t1 inner join Customer as t2 on t1.CustomerId = t2.CustomerId WHERE SalesOrderId = " + ID).FirstOrDefault();
+                        return File(byteexcel, "application/vnd.ms-excel", "Báo giá thông thường " + TENKH + ".xlsx");
+                    }
+                }
+            }
+        }
+        public ExcelWorksheet CoppyRow(ExcelWorksheet ws, int form, int to)
+        {
+            //   ws.Cells[String.Format("{0}:{0}", form)].Copy(ws.Cells[String.Format("{0}:{0}", to)]);
+            ws.Cells[form, 1, form, ws.Dimension.End.Column].Copy(ws.Cells[to, 1, to, ws.Dimension.End.Column]);
+            ws.Row(to).Height = ws.Row(form).Height;
+            ws.Row(to).StyleID = ws.Row(form).StyleID;
+            return ws;
+        }
+
         public ActionResult XuatBaoGia(int ID = 0)
         {
             ViewBag.ID = ID;
